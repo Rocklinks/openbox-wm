5ded977448bdcefc32d987770a6ad05c141b8afa
Rocklin K S <rocklin2603@gmail.com>
2024-07-13 11:57:27 +0530

    Add files via upload

---
+++ ubuntu 24.04/polybar/scripts/updates.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if ! updates=$(apt list --upgradable 2> /dev/null | grep -c upgradable); then
+    updates=0
+fi
+
+if [ "$updates" -gt 0 ]; then
+    echo "  $updates"
+else
+    echo "󰁟  $updates"
+fi
